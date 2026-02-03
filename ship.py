724b3de21f57d7e213cc01415d4ed50ceb5528a8
Val916 <junsofdev@proton.me>
2026-02-03 13:54:24 +0000

    Refactor ship movement logic to use ship speed setting and update position correctly

--- ship.py
+++ ship.py
@@ -16,17 +16,23 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on movement flag."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
 
+        # Update rect object from self.x.
+        self.rect.x = self.x
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
