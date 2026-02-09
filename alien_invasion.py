9f92098520508a296c498c27de2d6026d99ccb3b
Val916 <junsofdev@proton.me>
2026-02-09 21:31:07 +0000

    Add collision detection for bullets and aliens; implement fleet edge checking and direction change

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
             
     def _check_events(self):
         # Respond to keypresses and mouse events.
@@ -117,8 +121,23 @@ class AlienInvasion:
         pygame.display.flip()
 
     def _update_aliens(self):
-        # Check if the fleet is at an edge, and then update the positions of all aliens in the fleet.
+        # Check if the fleet is at an edge, then update the positions of all aliens in the fleet.
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        # Respond appropriately if any aliens have reached an edge.
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        # Drop the entire fleet and change the fleet's direction.
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+    
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
