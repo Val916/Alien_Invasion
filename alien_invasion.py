5f8a781d89c3e07ccf0819b00961da1b90bbf821
Val916 <junsofdev@proton.me>
2026-02-09 21:38:51 +0000

    Add collision detection for aliens and the ship; log a message on collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,7 +78,7 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
-            
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -132,6 +132,10 @@ class AlienInvasion:
         # Check if the fleet is at an edge, then update the positions of all aliens in the fleet.
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!")
 
     def _check_fleet_edges(self):
         # Respond appropriately if any aliens have reached an edge.
