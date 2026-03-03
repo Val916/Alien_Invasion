2c2384f6b4d1eb49dfab03f67905eb5abe3c3712
Val916 <junsofdev@proton.me>
2026-03-03 13:04:24 +0000

    Add level increment and rendering after fleet reset

--- alien_invasion.py
+++ alien_invasion.py
@@ -100,7 +100,7 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
-            
+
             # Increase level.
             self.stats.level += 1
             self.sb.prep_level()
@@ -190,6 +190,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
